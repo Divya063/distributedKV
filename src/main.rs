37da27fc0524b1fb775c7e9bf247f273fa2dc505
Divya Rani <drani@vmware.com>
2023-11-04 09:00:03 +0530

    move raft implementation to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 mod raft_impl;
-
-use raft::prelude::{Entry, EntryType};
-use std::{sync::mpsc::{channel, RecvTimeoutError, RecvError}, time::{Instant, Duration}};
-use std::thread;
-use raft_impl::Node;
-use std::sync::mpsc;
-use regex::Regex;
+use slog::{Drain, Logger};
 use std::collections::HashMap;
+use std::sync::mpsc::{self, RecvTimeoutError};
+use std::thread;
+use std::time::{Duration, Instant};
 
+use raft::eraftpb::ConfState;
+use raft::prelude::*;
+use raft::storage::MemStorage;
+use slog_term;
+use slog_async;
+use std::sync::mpsc::Sender;
+use std::sync::{Arc, Mutex};
+
+use raft_impl::*;
+
+use slog::{info, o};
 
 type ProposeCallback = Box<dyn Fn() + Send>;
 
@@ -15,150 +23,29 @@ enum Msg {
     Propose {
         id: u8,
         data: Vec<u8>,
-        callback: ProposeCallback,
+        cb: ProposeCallback,
     },
+    #[allow(dead_code)]
+    Raft(Message),
 }
+
 fn main() {
-    let mut node = Node::create_leader_node();
-  
-    let (tx, rx) = channel();
 
-    // Raft is a state machine, it will not change its state until the timeout.
-    // so we need to call the tick function to change the state of the Raft.
-    let timeout = Duration::from_millis(100);
-    let mut remaining_timeout = timeout;
+    let mut leader_node = Node::create_leader_node();
+    let node = Arc::new(Mutex::new(leader_node));
+    let sender = node.lock().unwrap().sender.clone();
 
-    let sender_thread = thread::spawn(move || loop {
-        let tx_clone = tx.clone();
-        send_propose(tx_clone, "Hello".to_string(), "World".to_string());
-    });
+    // to avoid blocking the main thread, we run the Raft in another thread.
+    let handler2 = thread::spawn(move || {raft_impl::start(node)});
+    let key = "foo".to_owned();
+    let value = "bar".to_owned();
+    put(sender, key, value);
+    
 
-   
-    let handle_1 = thread::spawn(move || loop {
-    let now = Instant::now();
-    let message = rx.recv();
-    // Use a HashMap to hold the `propose` callbacks.
-    let mut cbs = HashMap::new();
-    match message  {
-        Ok(Msg::Propose{id,data, callback}) => {
-            let last_index1 = node.node.raft.raft_log.last_index() + 1;
-            cbs.insert(id, callback);
-            let _result = node.node.propose(vec![], data);
-            let last_index2 = node.node.raft.raft_log.last_index() + 1;
-            if last_index2 == last_index1 {
-                println!("propose failed");
-            }
-            
-        },
-        Err(RecvError) => (),
-    }
 
-    let elapsed = now.elapsed();
-    if elapsed >= remaining_timeout {
-        remaining_timeout = timeout;
-        
-        node.node.tick();
-    } else {
-        remaining_timeout -= elapsed;
-    }
-
-    on_ready(&mut node, &mut cbs);
-
-});
-
-handle_1.join().unwrap();
-sender_thread.join().unwrap();
-
+    handler2.join().unwrap(); 
 }
 
-
-fn on_ready(node: &mut Node, cbs: &mut HashMap<u8, ProposeCallback>) {
-    if !node.node.has_ready() {
-        // Nothing to do.
-        return;
-    }
-
-    println!("node {}", node.node.has_ready());
-
-    let store = node.node.raft.raft_log.store.clone();
-
-    let mut ready = node.node.ready();
-    if !ready.snapshot().is_empty() {
-        let s = ready.snapshot().clone();
-        if let Err(e) = store.wl().apply_snapshot(s) {
-            println!("apply snapshot fail: {:?}, need to retry or panic",e);
-            return;
-        }
-    }
-    let committed_entries = ready.take_committed_entries();
-    println!("committed_entries {:?}", committed_entries);
-    if !committed_entries.is_empty() {
-        handle_committed_entries(node, committed_entries, cbs);
-        
-    }
-
-    if let Some(hs) = ready.hs() {
-        store.wl().set_hardstate(hs.clone());
-    }
-
-    // Advance the Raft.
-    let mut light_rd = node.node.advance(ready);
-    // Update commit index.
-    if let Some(commit) = light_rd.commit_index() {
-        store.wl().mut_hard_state().set_commit(commit);
-    }
-
-    handle_committed_entries(node, light_rd.take_committed_entries(), cbs);
-    // Advance the apply index.
-    node.node.advance_apply();
-
-   
-}
-
-pub fn handle_committed_entries(node: &mut Node, entries : Vec<Entry>, cbs: &mut HashMap<u8, ProposeCallback>) {
-    for entry in entries {
-        if entry.data.is_empty() {
-            // From new elected leaders.
-            continue;
-        }
-        if let EntryType::EntryConfChange = entry.get_entry_type() {
-            
-        } else {
-            let data = std::str::from_utf8(&entry.data).unwrap();
-            let reg = Regex::new("put ([0-9]+) (.+)").unwrap();
-            if let Some(caps) = reg.captures(data) {
-                node.key_value_store.insert(caps[1].parse().unwrap(), caps[2].to_string());
-                println!("cbs {}", entry.data.first().unwrap());
-                if let Some(cb) = cbs.remove(entry.data.first().unwrap()) {
-                    cb();
-                }
-            }
-        }
-
-    }
-}
-// call propose to add the request to the Raft log explicitly.
-fn send_propose(sender: mpsc::Sender<Msg>, key : String, value : String) {
-    thread::sleep(Duration::from_millis(1000));
-
-        let (tx, rx) = mpsc::sync_channel::<bool>(1);
-
-        let data = format!("put {} {}", key, value).into_bytes();
-
-        // Send a command to the Raft, wait for the Raft to apply it
-        // and get the result.
-        println!("Message sent to Raft.");
-        sender
-            .send(Msg::Propose{
-                id: 1,
-                data: data,
-                callback: Box::new(move || {
-                    tx.send(true).unwrap();
-                }),
-                })
-            .unwrap();
-        // let n = rx.recv().unwrap();
-        // assert_eq!(n, true);
-
-        println!("receive the propose callback");
+fn put(sender: Sender<raft_impl::Msg> , key: String, value: String){
+    raft_impl::send_propose(sender, key, value);
 }
--- src/raft_impl.rs
+++ src/raft_impl.rs
@@ -2,10 +2,39 @@ use raft::prelude::{RawNode,Config};
 use raft::storage::MemStorage;
 use raft::eraftpb::Snapshot;
 use::std::collections::HashMap;
+use slog::{Drain, o, info};
+
+use std::thread;
+use std::time::{Duration, Instant};
+
+use raft::prelude::*;
+use slog_term;
+use slog_async;
+use std::sync::mpsc;
+use std::sync::mpsc::RecvTimeoutError;
+use slog::Logger;
+use std::sync::{Arc, Mutex}; 
+
+
+type ProposeCallback = Box<dyn Fn() + Send>;
+
+pub enum Msg {
+    Propose {
+        id: u8,
+        data: Vec<u8>,
+        cb: ProposeCallback,
+    },
+    #[allow(dead_code)]
+    Raft(Message),
+}
+
 
 pub struct Node {
     pub node: RawNode<MemStorage>,
+    pub logger: slog::Logger,
     pub key_value_store: HashMap<String, String>,
+    pub sender: mpsc::Sender<Msg>,
+    pub receiver: mpsc::Receiver<Msg>,
 }
 
 impl Node {
@@ -14,24 +43,39 @@ impl Node {
             id: 1,
             ..Default::default()
         };
-        let logger = raft::default_logger();
+        let decorator = slog_term::TermDecorator::new().build();
+        let drain = slog_term::FullFormat::new(decorator).build().fuse();
+        let drain = slog_async::Async::new(drain)
+            .chan_size(4096)
+            .overflow_strategy(slog_async::OverflowStrategy::Block)
+            .build()
+            .fuse();
+
+        let logger = slog::Logger::root(drain, o!());
         let storage = MemStorage::new();
         let mut s = Snapshot::default();
+        // state to start with
         s.mut_metadata().index = 1;
         s.mut_metadata().term = 1;
         s.mut_metadata().mut_conf_state().voters = vec![1];
 
         storage.wl().apply_snapshot(s).unwrap();
+        let (sender, receiver) = mpsc::channel();
         let mut node = RawNode::new(&config, storage, &logger).unwrap();
         
         // mandatory to become a candidate first: invalid transition [follower -> leader]
         node.raft.become_candidate();
         node.raft.become_leader();
-        Node {
+         Node {
             node: node,
+            logger: logger,
+            sender: sender,
+            receiver: receiver,
             key_value_store: HashMap::new(),
-        }
+         }
+
     }
+
 
     pub fn create_follower_node()-> Self{
         let config = Config {
@@ -41,11 +85,160 @@ impl Node {
         let logger = raft::default_logger();
         let storage = MemStorage::new();
         let mut node = RawNode::new(&config, storage, &logger).unwrap();
-        node.raft.become_follower(1, 1);
+        let (sender, receiver) = mpsc::channel();
         Node {
             node: node,
+            logger: logger,
+            sender: sender,
+            receiver: receiver,
             key_value_store: HashMap::new(),
         }
     }
+
+    pub fn get_value(&self, key: &str) -> Option<String> {
+        self.key_value_store.get(key).cloned()
+    }
+   
+}
+
+pub fn start(node: Arc<Mutex<Node>>) {
+    let mut cbs = HashMap::new();
+    let cloned_node = Arc::clone(&node);
+    loop {
+         // Loop forever to drive the Raft.
+        let mut node = cloned_node.lock().unwrap();
+        let mut t = Instant::now();
+        let mut timeout = Duration::from_millis(100);
+            match node.receiver.recv_timeout(timeout) {
+                Ok(Msg::Propose { id, data, cb }) => {
+                    print!(" receive data: {}", std::str::from_utf8(&data).unwrap());
+                    cbs.insert(id, cb);
+                    node.node.propose(vec![], data).unwrap();
+                }
+                Ok(Msg::Raft(m)) => node.node.step(m).unwrap(),
+                Err(RecvTimeoutError::Timeout) => (),
+                Err(RecvTimeoutError::Disconnected) => return,
+            }
+    
+            let d = t.elapsed();
+            t = Instant::now();
+            if d >= timeout {
+                timeout = Duration::from_millis(100);
+                // We drive Raft every 100ms.
+                node.node.tick();
+            } else {
+                timeout -= d;
+            }
+        on_ready(&mut node, &mut cbs);
+        }
+ 
+}
+
+fn on_ready(node: &mut Node,cbs: &mut HashMap<u8, ProposeCallback>) {
+    let logger = node.logger.clone();
+    if !node.node.has_ready() {
+        return;
+    }
+    let store = node.node.raft.raft_log.store.clone();
+
+    // Get the `Ready` with `RawNode::ready` interface.
+    let mut ready = node.node.ready();
+
+    let handle_messages = |msgs: Vec<Message>| {
+        for _msg in msgs {
+            // Send messages to other peers.
+        }
+    };
+
+    if !ready.messages().is_empty() {
+        // Send out the messages come from the node.
+        handle_messages(ready.take_messages());
+    }
+
+    if !ready.snapshot().is_empty() {
+        // This is a snapshot, we need to apply the snapshot at first.
+        store.wl().apply_snapshot(ready.snapshot().clone()).unwrap();
+    }
+
+    let mut _last_apply_index = 0;
+    let mut handle_committed_entries = |committed_entries: Vec<Entry>| {
+        info!(logger, "handle_committed_entries"; "committed_entries" => format!("{:?}", committed_entries));
+        for entry in committed_entries {
+
+            if entry.data.is_empty() {
+                // Empty entry, when the peer becomes Leader it will send an empty entry.
+                continue;
+            }
+
+            if entry.get_entry_type() == EntryType::EntryNormal {
+                let data = std::str::from_utf8(&entry.data).unwrap();
+                let temp_data: Vec<&str> = data.split(" ").collect();
+                let key = temp_data[0].to_owned();
+                let value = temp_data[1].to_owned();
+                info!(logger, "key {} value {}", key, value);
+                node.key_value_store.insert(key, value);
+                info!(logger, "handle_committed_entries"; "data" => data);
+                if let Some(cb) = cbs.remove(entry.data.first().unwrap()) {
+                    cb();
+                }
+            }
+
+            // TODO: handle EntryConfChange
+        }
+    };
+    handle_committed_entries(ready.take_committed_entries());
+
+    if !ready.entries().is_empty() {
+        // Append entries to the Raft log.
+        store.wl().append(ready.entries()).unwrap();
+    }
+
+    if let Some(hs) = ready.hs() {
+        // Raft HardState changed, and we need to persist it.
+        store.wl().set_hardstate(hs.clone());
+    }
+
+    if !ready.persisted_messages().is_empty() {
+        // Send out the persisted messages come from the node.
+        handle_messages(ready.take_persisted_messages());
+    }
+
+    // Advance the Raft.
+    let mut light_rd = node.node.advance(ready);
+    // Update commit index.
+    if let Some(commit) = light_rd.commit_index() {
+        store.wl().mut_hard_state().set_commit(commit);
+    }
+    // Send out the messages.
+    handle_messages(light_rd.take_messages());
+    // Apply all committed entries.
+    handle_committed_entries(light_rd.take_committed_entries());
+    // Advance the apply index.
+    node.node.advance_apply();
+}
+
+pub fn send_propose(sender: mpsc::Sender<Msg>, key: String, value: String) {
+        let logger = raft::default_logger();
+
+        let (s1, r1) = mpsc::channel::<u8>();
+        let data = format!("{} {}", key, value).into_bytes();
+
+        // Send a command to the Raft, wait for the Raft to apply it
+        // and get the result.
+        sender
+            .send(Msg::Propose {
+                id: 1,
+                data: data,
+                cb: Box::new(move || {
+                    s1.send(0).unwrap();
+                }),
+            })
+            .unwrap();
+        let n = r1.recv().unwrap();
+        assert!(n == 1);
+        info!(logger, "receive the propose callback"; "n" => n);
+        if n == 0 {
+            info!(logger, "receive the propose callback");
+        }
 }
 
